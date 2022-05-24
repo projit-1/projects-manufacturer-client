c7fc25b146d5052ac37c6dbbd38c7347f8e720a4
projit-1 <projitmondol@gmail.com>
2022-05-24 10:41:33 +0600

    blogs question 4 is added

--- src/Pages/Home/Blogs.js
+++ src/Pages/Home/Blogs.js
@@ -1,18 +1,91 @@
 import React from 'react';
+import questionimg from '../../Assets/questionimg.png'
 
 const Blogs = () => {
     return (
         <div>
-            <h2> How will I improve the performance of a React Application?</h2>
-            <p>Optimizing operation performance is crucial for inventors who are aware of keeping a stoner’s experience positive to keep them on an app and engaged. According to exploration by Akamai, a alternate detention in cargo time can beget a 7 percent reduction in transformations, making it imperative for inventors to produce apps with optimized performance. In React operations, we're guaranteed a veritably fast UI by dereliction. Still, as an operation grows, inventors may encounter some performance issues. In this companion, we will bandy five important ways to optimize the performance of a React operation, includingpre-optimization ways. These include</p>
-            <p>•	By keeping component state local where necessary,
-                <br />
-                •	By memoizing React components to prevent unnecessary re-renders,
-                <br />
-                •	By code-splitting in React using dynamic import(), and
-                <br />
-                •	By windowing or list virtualization in React
-            </p>
+            <div className='mx-10'>
+                <h2 className='text-2xl'> 1. How will I improve the performance of a React Application?</h2> <br />
+                <p className='text-justify'>Optimizing operation performance is crucial for inventors aware of keeping a stoner’s experience positive to keep them on an app and engaged. According to exploration by Akamai, alternate detention in cargo time can beget a 7 percent reduction in transformations, making it imperative for inventors to produce apps with optimized performance. In React operations, we're guaranteed a veritably fast UI by dereliction. Still, as an operation grows, inventors may encounter some performance issues. In this companion, we will bandy five important ways to optimize the performance of a React operation, including-optimization ways. These include</p>
+                <p>•	By keeping component state local where necessary,
+                    <br />
+                    •	By memoizing React components to prevent unnecessary re-renders,
+                    <br />
+                    •	By code-splitting in React using dynamic import(), and
+                    <br />
+                    •	By windowing or list virtualization in React
+                </p>
+            </div> <br />
+            <div className='mx-10'>
+                <h2 className='text-2xl'> 2. What are the different ways to manage a state in a React application? </h2> <br />
+                <p className='text-justify'>The state helps in keeping the data of different factors in sync since each state update will render all applicable factors. It can also act as a medium to communicate between colorful factors. Managing state is one of the hardest corridors of any operation, and that's why there are so numerous state operation libraries/ tools available, including Redux, MobX, Flux, RxJS, and more.
+                    Everything has some graces and faults, and being these and with a lot of trial and error, airman programs, and particular observance, it has been proven that React Native apps can be structured into 5 types of state. Each type of state follows a set of defined rules and interacts with one another in a particular manner.
+                    Grounded on these rules of each state, you can fluently store data as per your requirements (as long as you follow the rules) without fussing about creating a dynamic data mess.
+                </p> <br />
+                <p>(a).	Communication state forms the backbone of your React Native app without you indeed knowing about it. Remember when you had requested a call back to an HTTP request? That’s when you introduced the communication system in your app. </p>
+                <br /><p>
+                    (b ). Data state covers information that your React operation stores temporarily for colorful business functions. Apparently, you're erecting a design operation app. The information stored in the data state will include the following effects – design names, connections, details about the guests, etc. </p>
+                <br /><p>
+                    (c). Contrary to the state mentioned over in a React app, the control state doesn't represent the operation’s terrain. Rather, it refers to the state that the stoner has input into the app. For illustration, form inputs, named particulars, etc. The Control state is known to be more different and protean when it comes to storing information. </p>
+                <br /><p>
+                    (d). Session state contains information about the stoner of the operation similar to stoner id, warrants, watchwords, etc. It may also include information on how the operation should work according to a particular stoner’s preferences. While the Session state can store analogous patterned factors like the Control state, there's a thick difference between both the information stored. For illustration, you may have a part of Control state information that represents the corridor of a tree view, you can find kind of analogous data in the Session state, but it'll surely be different from the Control state. </p>
+                <br /><p>
+                    (e). Position state is the UTF-8 display that appears in your URL bar. In fact, the L in URL stands for Locator! One of the most intriguing data about Position state is that you can give directions to a stoner to the corridor of the operation that doesn't have unique URLs associated with them. Also, the HTML5 History API allows you to store countries independently from the specific URL.
+                </p>
+            </div><br />
+            <div className='mx-10'>
+                <h2 className='text-2xl'> 3. How does prototypical inheritance work? </h2> <br />
+                <p className='text-justify'>Prototypal inheritance suggests that a relationship that is true once a straightforward perform JavaScript is simply objects and not categories, I differ with the word inheritance itself, as that name implies (from classical languages) a copy-operation wherever behavior from the "parent" is duplicated into the "child". This can be not the least bit however JavaScript works.
+                </p> <br />
+                <p>Prototypical inheritance works by the subsequent ways in which, </p>
+                <br /><p>
+                    In a class-based model, you have got categories that are drawn by the triple “. Where: </p>
+                <br /><p>
+                    Parents are the list of categories you’re extending. Categories could solely extend alternative classes; </p>
+                <br /><p>
+                    Variables are the variety of variable slots that instances can have. As an example, a “class Point2d (int x, int y)” has a pair of instance variables; </p>
+                <br /><p>
+                    Methods could be a table of “name → function” that describes the services every instance of the category can support; <br />
+                    Instances (or Objects) in an exceedingly class-based model are drawn with the tuple “. Where:
+                </p>
+                <br /><p>
+                    Class could be a pointer to the category triple that defines what percentage variables this instance supports, and what strategies you'll turn it;
+                </p>
+                <br /><p>
+                    Values could be a list of the values for every variable the instance has.
+                </p>
+                <br /><p>
+                    In this model, categories solely describe however instances appear as if, and Instances are the sole factor you'll move with. Categories can't be instances, and you can’t inherit from Instances.
+                </p> <br />
+                <p>We can explane it by the following image:
+                    <img className='mx-auto' src={questionimg} alt="" />
+                    <p className='text-accent mx-20'> Image is collected from internet.</p>
+                </p>
+            </div> <br />
+            <div className='mx-10'>
+                <h2 className='text-2xl'> 6. What is a unit test? Why should write unit tests? </h2> <br />
+                <h2 className='text-xl'>  What is Unit Testing? </h2> <br />
+
+                <p className='text-justify'>Unit testing is a type of software testing where individual units or factors of software are tested. The purpose is to validate that each unit of the software law performs as anticipated. Unit Testing is done during the development ( rendering phase) of an operation by the inventors. Unit Tests insulate a section of law and corroborate its correctness. A unit may be an individual function, system, procedure, module, or object. In SDLC, STLC, V Model, Unit testing is first position of testing done before integration testing. Unit testing is a WhiteBox testing fashion that's generally performed by the inventor. Though, in a practical world due to time crunch or disinclination of inventors to tests, QA masterminds also do unit testing.
+                </p> <br />
+                <h2 className='text-xl'>  Why should write unit tests? </h2>
+                <br /><p>
+                    There are many benefits of unit testing. These are written below:</p>
+                <br /><p>
+                    The main benefit of unit testing is the process becomes nimble. </p>
+                <br /><p>
+                    Unit testing significantly improves law quality. It helps inventors to identify the lowest blights that might be present in the units before they go for integration testing. </p>
+                <br /><p>
+                    Unit testing helps identify all kinds of issues with the software at a veritably early stage. Software inventors can also work on those issues first before progressing any farther.<br />
+                    The main advantage of this is when the issues are resolved at an early stage, no other part of the software is impacted.
+                </p>
+                <br /><p>
+                    Refactoring the law or streamlining the system library becomes much easier when you test each element of the softwareindividually.However, they're detected beforehand on and making changes to the system therefore becomes important easier, If there are any problems. The delicacy of each unit is vindicated before it moves on to the coming phase.
+                </p>
+                <br /><p>
+                    The debugging process can be simplified to a great extent by unittesting.However, also only the rearmost changes that have been made to the law need to be amended.
+                </p>
+            </div>
         </div>
     );
 };
